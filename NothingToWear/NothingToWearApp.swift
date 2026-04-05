79d2042b13afd030ea6bbf6c5692e4cca850b097
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2026-04-05 16:15:53 +0900

    feature:ios프로젝트 초기세팅

---
+++ NothingToWear/NothingToWearApp.swift
@@ -0,0 +1,20 @@
+//
+//  NothingToWearApp.swift
+//  NothingToWear
+//
+//  Created by 장아연 on 4/5/26.
+//  Created by 장아연 on 4/5/26.
+//  Created by 장아연 on 4/5/26.
+//  Created by 장아연 on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct NothingToWearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
